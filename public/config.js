39f1e56ea76c8415fa82862ae2b14150f0c5500c
orphanlol <83834491+orphanlol@users.noreply.github.com>
2024-05-12 22:12:17 +0000

    dig

---
+++ public/config.js
@@ -0,0 +1,22 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
+  VITE_CORS_PROXY_URL: "https://loquacious-empanada-76496a.netlify.app",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYWQwMTNmMmY4ZTJjOTU3ZmNjNzU0MDNkNjIxZTQ0YiIsInN1YiI6IjY2NDEzNzMwY2IyZTNjZDgyMzk2MjJkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UE9YY5xehn8p3fs50_kR-JoZSsUOY1ueEH_mP2Pv8_M",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: true,
+
+  // The backend URL to communicate with
+  VITE_BACKEND_URL: "https://backend.sudo-flix.lol",
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: "",
+
+  VITE_ALLOW_AUTOPLAY: true,
+};
